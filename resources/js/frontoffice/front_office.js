5e3a1434bff7fd6f58ff2aa52529b951c6fa19de
paul <paul.anderson.b.garcia@gmail.com>
2021-08-08 11:00:25 +0800

    first commi

---
+++ resources/js/frontoffice/front_office.js
@@ -0,0 +1,19 @@
+require('../bootstrap');
+window.Vue = require('vue');
+
+// // import router from './src/router/index'
+import vuetify from '@front/plugin/vuetify/index'
+// import VueToast from 'vue-toast-notification';
+// import VeeValidate from 'vee-validate';
+
+// import 'vue-toast-notification/dist/index.css';
+// Vue.use(VeeValidate);
+// Vue.use(VueToast);
+const axios = require('axios');
+
+Vue.component('main-app', require('@front/layout/App.vue').default);
+
+const app = new Vue({
+    el:'#front_office',
+    vuetify
+});
---
+++ resources/js/frontoffice/src/layout/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>test 1</h1>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    },
+    computed:{
+
+    },
+    created(){
+
+    }
+}
+</script>
